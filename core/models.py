999a4a3ee0a9bd111d00684797e6e526c97b82e9
tristanmatthew <tristanmatthew@gmail.com>
2021-03-01 15:23:23 -0500

    render posts on page

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=280)
+    artist = models.CharField(max_length=280)
+    release_year = models.IntegerField(blank=True, null=True)     
+
+    def __str__(self):
+        return f"{self.title} | {self.artist} | {str(self.release_year)}"
